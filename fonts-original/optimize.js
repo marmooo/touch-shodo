eb29dec4d7fee70b2224421e46445cc7eee6a427
marmooo <marmooo@users.noreply.github.com>
2020-11-24 14:23:39 +0900

    cleanup

--- fonts-original/optimize.js
+++ fonts-original/optimize.js
@@ -8,6 +8,6 @@ glob('*.woff2', (err, paths) => {
     console.log(path);
     var new_path = '../fonts/' + path;
     // pip install fonttools
-    execSync(`pyftsubset ${path} --text-file=optimize.txt --layout-features='*' --flavor=woff2 --output-file=${new_path}    `, { cwd:__dirname });
+    execSync(`pyftsubset ${path} --text-file=optimize.txt --layout-features='*' --flavor=woff2 --output-file=${new_path}`, { cwd:__dirname });
   });
 });
