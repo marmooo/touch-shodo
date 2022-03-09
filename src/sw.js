5212b77ebb3b108e2247d852b2b7cdcec92f4cac
marmooo <marmooo@users.noreply.github.com>
2022-03-10 00:50:51 +0900

    fix path

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-02-08 10:40';
+var CACHE_NAME = '2022-03-10 00:50';
 var urlsToCache = [
   "/touch-shodo/",
   "/touch-shodo/index.js",
@@ -12,7 +12,7 @@ var urlsToCache = [
   "/touch-shodo/mp3/stupid5.mp3",
   "/touch-shodo/favicon/original.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.2/dist/signature_pad.umd.min.js",
 ];
 
