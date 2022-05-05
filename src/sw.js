bc16846ac99a41a241ff29541e8639177296ea8c
marmooo <marmooo@users.noreply.github.com>
2022-05-06 07:59:35 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-09 00:05';
+var CACHE_NAME = '2022-05-06 08:00';
 var urlsToCache = [
   "/touch-shodo/",
   "/touch-shodo/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/touch-shodo/mp3/correct3.mp3",
   "/touch-shodo/mp3/incorrect1.mp3",
   "/touch-shodo/mp3/stupid5.mp3",
-  "/touch-shodo/favicon/original.svg",
+  "/touch-shodo/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
