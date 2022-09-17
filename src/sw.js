073d9548416b85ad5082590147fe081d5eee096d
marmooo <marmooo@users.noreply.github.com>
2022-09-17 12:28:26 +0900

    bump signature_pad from 4.0.7 to 4.0.8

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-09-11 08:49";
+var CACHE_NAME = "2022-09-17 12:28";
 var urlsToCache = [
   "/touch-shodo/",
   "/touch-shodo/index.js",
@@ -13,7 +13,7 @@ var urlsToCache = [
   "/touch-shodo/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.7/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.8/dist/signature_pad.umd.min.js",
 ];
 
 self.addEventListener("install", function (event) {
