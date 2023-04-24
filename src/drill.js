7e14a7070e8c0ff4912701959f20e6e01e982395
marmooo <marmooo@users.noreply.github.com>
2023-04-24 09:09:39 +0900

    cleanup

--- src/drill.js
+++ src/drill.js
@@ -42,11 +42,13 @@ const dirNames = [
   "常用",
   "常用外",
 ];
-let level = 2;
-let correctAudio, incorrectAudio, correctAllAudio, stupidAudio;
-loadAudios();
-const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
+const audioBufferCache = {};
+loadAudio("stupid", "/touch-shodo/mp3/stupid5.mp3");
+loadAudio("correct", "/touch-shodo/mp3/correct3.mp3");
+loadAudio("correctAll", "/touch-shodo/mp3/correct1.mp3");
+loadAudio("incorrect", "/touch-shodo/mp3/incorrect1.mp3");
+let level = 2;
 let prevCanvasSize;
 let canvasSize = 140;
 let maxWidth = 4;
@@ -138,52 +140,33 @@ function toggleScroll() {
   }
 }
 
-function playAudio(audioBuffer, volume) {
-  const audioSource = audioContext.createBufferSource();
-  audioSource.buffer = audioBuffer;
+async function playAudio(name, volume) {
+  const audioBuffer = await loadAudio(name, audioBufferCache[name]);
+  const sourceNode = audioContext.createBufferSource();
+  sourceNode.buffer = audioBuffer;
   if (volume) {
     const gainNode = audioContext.createGain();
     gainNode.gain.value = volume;
     gainNode.connect(audioContext.destination);
-    audioSource.connect(gainNode);
-    audioSource.start();
+    sourceNode.connect(gainNode);
+    sourceNode.start();
   } else {
-    audioSource.connect(audioContext.destination);
-    audioSource.start();
+    sourceNode.connect(audioContext.destination);
+    sourceNode.start();
   }
+}
+
+async function loadAudio(name, url) {
+  if (audioBufferCache[name]) return audioBufferCache[name];
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  audioBufferCache[name] = audioBuffer;
+  return audioBuffer;
 }
 
 function unlockAudio() {
   audioContext.resume();
-}
-
-function loadAudio(url) {
-  return fetch(url)
-    .then((response) => response.arrayBuffer())
-    .then((arrayBuffer) => {
-      return new Promise((resolve, reject) => {
-        audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
-          resolve(audioBuffer);
-        }, (err) => {
-          reject(err);
-        });
-      });
-    });
-}
-
-function loadAudios() {
-  promises = [
-    loadAudio("/touch-shodo/mp3/correct3.mp3"),
-    loadAudio("/touch-shodo/mp3/incorrect1.mp3"),
-    loadAudio("/touch-shodo/mp3/correct1.mp3"),
-    loadAudio("/touch-shodo/mp3/stupid5.mp3"),
-  ];
-  Promise.all(promises).then((audioBuffers) => {
-    correctAudio = audioBuffers[0];
-    incorrectAudio = audioBuffers[1];
-    correctAllAudio = audioBuffers[2];
-    stupidAudio = audioBuffers[3];
-  });
 }
 
 customElements.define(
