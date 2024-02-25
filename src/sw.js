ff6a4620ecd3ac04ff22e5d9234cc3b7e3fba14e
marmooo <marmooo@users.noreply.github.com>
2024-02-25 09:47:59 +0900

    bump bootstrap from 5.3.2 to 5.3.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-02-16 00:20";
+const CACHE_NAME = "2024-02-25 09:42";
 const urlsToCache = [
   "/touch-shodo/",
   "/touch-shodo/index.js",
@@ -9,7 +9,7 @@ const urlsToCache = [
   "/touch-shodo/mp3/incorrect1.mp3",
   "/touch-shodo/mp3/stupid5.mp3",
   "/touch-shodo/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js",
 ];
 
