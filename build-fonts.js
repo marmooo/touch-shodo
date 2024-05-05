0dd3bc0e3d0dcf42af6231df73619e6201cc1d35
marmooo <marmooo@users.noreply.github.com>
2024-05-06 00:05:52 +0900

    use fontconv

--- build-fonts.js
+++ build-fonts.js
@@ -1,37 +1,31 @@
 import { basename } from "https://deno.land/std/path/mod.ts";
 import { expandGlobSync } from "https://deno.land/std/fs/expand_glob.ts";
-import { ttf2svgFont } from "@marmooo/ttf2svg";
-import { JKAT } from "@marmooo/kanji";
-import svg2ttf from "svg2ttf";
-import ttf2woff2 from "ttf2woff2";
+import { convert } from "npm:fontconv@0.0.3";
+import { JKAT } from "npm:@marmooo/kanji@0.0.8";
 
-function buildPreview() {
+async function buildPreview() {
   const text = "臨機応変疾風迅雷明鏡止水一糸不乱伝家宝刀奇想天外";
   for (const file of expandGlobSync("fonts/*.ttf", { globstar: true })) {
     const ttf = Deno.readFileSync(file.path);
-    const svg = ttf2svgFont(ttf, { text });
-    const woff2 = ttf2woff2(svg2ttf(svg).buffer);
-
+    const woff2 = await convert(ttf, ".woff2", { text, removeLigatures: true });
     const name = basename(file.path).split(".")[0];
     const outPath = `src/fonts/${name}-preview.woff2`;
     Deno.writeFileSync(outPath, woff2);
   }
 }
 
-function buildWoff2() {
+async function buildWoff2() {
   const hirakanas = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵ";
   const kanjis = JKAT.slice(0, 10).flat().join("");
   const text = hirakanas + kanjis;
   for (const file of expandGlobSync("fonts/*.ttf", { globstar: true })) {
     const ttf = Deno.readFileSync(file.path);
-    const svg = ttf2svgFont(ttf, { text });
-    const woff2 = ttf2woff2(svg2ttf(svg).buffer);
-
+    const woff2 = await convert(ttf, ".woff2", { text, removeLigatures: true });
     const name = basename(file.path).split(".")[0];
     const outPath = `src/fonts/${name}.woff2`;
     Deno.writeFileSync(outPath, woff2);
   }
 }
 
-buildPreview();
-buildWoff2();
+await buildPreview();
+await buildWoff2();
